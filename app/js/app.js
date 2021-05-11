951cc288546dab0c72f6e4b4a4c8c350a5d1ba81
avasdowney <avasdowney@gmail.com>
2021-05-11 17:23:58 -0400

    added diamond

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,29 @@
 window.onload = () => {
-    console.log(`%c JavaScript works!`,
-        `background-color: yellow; font-weight: bold;`);
+
+	var diamond =  ``;
+    let diamondDiameter = parseInt(prompt(`Enter the radius of the diamond: `, `5`));
+
+    // creates the diamond shape
+    function createDiamond(diameter) {
+        // bottom half
+        for(var row=1; row<=diameter; row++) {
+	        for(var col=1; col<=2*row-1; col++) {
+	            diamond += `*`;
+	        }
+	        diamond += '<br>';
+	    } 
+        // top half
+	    for(var row=diameter; row>=1; row--) {
+	        for(var col=2; col<2*row-1; col++) {
+	            diamond += `*`;
+	        }
+	        diamond += '<br>';
+	    } 
+		// print full diamond
+		document.querySelector(`div`).innerHTML = diamond;
+    };
+
+	// call function to create diamond
+    createDiamond(diamondDiameter);
+
 };
